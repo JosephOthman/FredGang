21b391153d68631f718b1aa2d3efa0f4ccd6e486
joe othman <jothman30@stuy.edu>
2022-01-23 15:54:14 -0500

    almost there!

---
+++ v5/Connect4.java
@@ -0,0 +1,160 @@
+public class Connect4{
+  public String[][] board = new String[6][7];
+  public int moves = 0;
+
+
+  public void clearBoard(){
+    for(int i=0; i<6; i++){
+      for(int j=0; j<7; j++){
+        board[i][j] = ".";
+      }
+    }
+  }
+
+  public void resetGame(){
+    moves = 0;
+    clearBoard();
+  }
+
+  public boolean dropO(int x){
+    boolean hasWon = false;
+    int column=x;
+    int row=5;
+    for(int j=5; j>=0; j--){
+      if(!(board[j][x].equals("."))){
+        row--;
+      }
+    }
+    board[row][x] = "O";
+    String checker = "OOOO";
+    String horiz = "";
+    String vert = "";
+    String diag1 = "";
+    String diag2 = "";
+    for(int j=5; j>=row; j--){
+      vert+=board[j][x];
+    }
+    System.out.println(vert);
+    for(int j=0; j<7; j++){
+      horiz+=board[row][j];
+    }
+    System.out.println(horiz);
+    int c1 = column;
+    int r1 = row;
+    int c2 = column;
+    int r2 = row;
+    while((c1>0 && r1>0)){
+      c1--;
+      r1--;
+    }
+    while((c1<=6 && r1<=5)){
+      diag1+=board[r1][c1];
+      c1++;
+      r1++;
+    }
+    System.out.println("diag1:" + diag1);
+
+    while((c2<6 && r2>0)){
+      c2++;
+      r2--;
+    }
+    while((c2>=0 && r2<=5)){
+      diag2+=board[r2][c2];
+      c2--;
+      r2++;
+    }
+    System.out.println("diag2:" + diag2);
+    if(vert.indexOf(checker)>=0){
+      hasWon = true;
+    }
+    if(horiz.indexOf(checker)>=0){
+      hasWon = true;
+    }
+    if(diag1.indexOf(checker)>=0){
+      hasWon = true;
+    }
+    if(diag2.indexOf(checker)>=0){
+      hasWon = true;
+    }
+    return hasWon;
+  }
+
+  public boolean dropX(int x){
+    boolean hasWon = false;
+    int column=x;
+    int row=5;
+    for(int j=5; j>=0; j--){
+      if(!(board[j][x].equals("."))){
+        row--;
+      }
+    }
+    board[row][x] = "X";
+    String checker = "XXXX";
+    String horiz = "";
+    String vert = "";
+    String diag1 = "";
+    String diag2 = "";
+    for(int j=5; j>=row; j--){
+      vert+=board[j][x];
+    }
+    System.out.println(vert);
+    for(int j=0; j<7; j++){
+      horiz+=board[row][j];
+    }
+    System.out.println(horiz);
+    int c1 = column;
+    int r1 = row;
+    int c2 = column;
+    int r2 = row;
+    System.out.println(column);
+    System.out.println(row);
+    while((c1>0 && r1>0)){
+      c1--;
+      r1--;
+      System.out.println(c1);
+      System.out.println(r1);
+    }
+    while((c1<=6 && r1<=5)){
+      diag1+=board[r1][c1];
+      c1++;
+      r1++;
+    }
+    System.out.println("diag1:" + diag1);
+
+    while((c2<6 && r2>0)){
+      c2++;
+      r2--;
+    }
+    while((c2>=0 && r2<=5)){
+      diag2+=board[r2][c2];
+      c2--;
+      r2++;
+    }
+    System.out.println("diag2:" + diag2);
+    if(vert.indexOf(checker)>=0){
+      hasWon = true;
+    }
+    if(horiz.indexOf(checker)>=0){
+      hasWon = true;
+    }
+    if(diag1.indexOf(checker)>=0){
+      hasWon = true;
+    }
+    if(diag2.indexOf(checker)>=0){
+      hasWon = true;
+    }
+    return hasWon;
+  }
+
+  public String boardRep(){
+    String s = "";
+    for(int i=0; i<6; i++){
+      for(int j=0; j<7; j++){
+        s += board[i][j] + " ";
+      }
+      s+="\n";
+    }
+    return s;
+  }
+
+}
---
+++ v5/Driver.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+
+  public String[][] board;
+  public int moves;
+  public static Connect4 game = new Connect4();
+
+  public static Scanner in = new Scanner(System.in);
+
+  public Driver(){
+    board = new String[6][7];
+    moves = 0;
+    NewGame();
+  }
+
+  public static void NewGame(){
+    String s = "";
+    s = "================================================================\n";
+    s += "Welcome! You are about to begin a game of Connect4\n";
+    System.out.println(s);
+    System.out.println("Columns are numbered left to right from 0-6");
+    game.resetGame();
+  }
+
+  public static boolean playTurn(){
+    System.out.println(game.boardRep());
+    System.out.println("Player 1, pick a column to place a piece");
+      try {
+        int O = Integer.parseInt(in.nextLine());
+        while((O<0) || (O>6)) {
+          System.out.println("Please enter an integer between 0 and 6, inclusive.");
+          O = Integer.parseInt(in.nextLine());
+        }
+          if(game.dropO(O)==true){
+            return true;
+          }
+      }
+      catch (Exception e ) { //If error is caught
+        System.out.println("An error has occured, plz try again.");
+      }
+      System.out.println(game.boardRep());
+      System.out.println("Player 2, pick a column to place a piece");
+      try {
+        int X = Integer.parseInt( in.nextLine());
+        while((X<0) || (X>6)) {
+          System.out.println("Please enter an integer between 0 and 6, inclusive.");
+          X = Integer.parseInt(in.nextLine());
+        }
+          if(game.dropX(X)==true){
+            return true;
+          }
+      }
+      catch (Exception e ) { //If error is caught
+        System.out.println("An error has occured, plz try again.");
+      }
+      return false;
+  }
+
+
+  public static void main(String[] args) {
+    NewGame();
+    while(!(playTurn())){
+      playTurn();
+    }
+    System.out.println("The game has ended");
+  }
+
+  //Arrays.deepToString(int[][])
+
+}
