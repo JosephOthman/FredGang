5c5e6a8ef42554b1db04faf8a51e8d16ff673945
LawrenceJoa <78170819+Law111@users.noreply.github.com>
2022-01-23 18:10:38 -0500

    Update Connect4.java

--- Connect4.java
+++ Connect4.java
@@ -27,50 +27,49 @@ public class Connect4{
     }
     board[row][x] = "O";
     String checker = "OOOO";
-    String vert = "";
     String horiz = "";
+    String vert = "";
     String diag1 = "";
     String diag2 = "";
     for(int j=5; j>=row; j--){
       vert+=board[j][x];
     }
-    if(vert.indexOf(checker)>=0){
-      hasWon = true;
-    }
     for(int j=0; j<7; j++){
       horiz+=board[row][j];
     }
-    if(horiz.indexOf(checker)>=0){
-      hasWon = true;
-    }
     int c1 = column;
     int r1 = row;
-    while((c1>=0 && r1>=0)){
+    int c2 = column;
+    int r2 = row;
+    while((c1>0 && r1>0)){
       c1--;
       r1--;
     }
-    int c2 = column;
-    int r2 = row;
-    while((c2<=6 && r2>=0)){
-      c2++;
-      r2--;
-    }
     while((c1<=6 && r1<=5)){
-      diag1+=board[c1][r1];
+      diag1+=board[r1][c1];
       c1++;
       r1++;
     }
+    while((c2<6 && r2>0)){
+      c2++;
+      r2--;
+    }
     while((c2>=0 && r2<=5)){
-      diag2+=board[c2][r2];
+      diag2+=board[r2][c2];
       c2--;
       r2++;
+    }
+    if(vert.indexOf(checker)>=0){
+      hasWon = true;
+    }
+    if(horiz.indexOf(checker)>=0){
+      hasWon = true;
     }
     if(diag1.indexOf(checker)>=0){
       hasWon = true;
     }
     if(diag2.indexOf(checker)>=0){
       hasWon = true;
-
     }
     return hasWon;
   }
@@ -86,43 +85,43 @@ public class Connect4{
     }
     board[row][x] = "X";
     String checker = "XXXX";
-    String vert = "";
     String horiz = "";
+    String vert = "";
     String diag1 = "";
     String diag2 = "";
     for(int j=5; j>=row; j--){
       vert+=board[j][x];
     }
-    if(vert.indexOf(checker)>=0){
-      hasWon = true;
-    }
     for(int j=0; j<7; j++){
       horiz+=board[row][j];
     }
-    if(horiz.indexOf(checker)>=0){
-      hasWon = true;
-    }
     int c1 = column;
     int r1 = row;
-    while((c1>=0 && r1>=0)){
+    int c2 = column;
+    int r2 = row;
+    while((c1>0 && r1>0)){
       c1--;
       r1--;
     }
-    int c2 = column;
-    int r2 = row;
-    while((c2<=6 && r2>=0)){
-      c2++;
-      r2--;
-    }
     while((c1<=6 && r1<=5)){
-      diag1+=board[c1][r1];
+      diag1+=board[r1][c1];
       c1++;
       r1++;
     }
+    while((c2<6 && r2>0)){
+      c2++;
+      r2--;
+    }
     while((c2>=0 && r2<=5)){
-      diag2+=board[c2][r2];
+      diag2+=board[r2][c2];
       c2--;
       r2++;
+    }
+    if(vert.indexOf(checker)>=0){
+      hasWon = true;
+    }
+    if(horiz.indexOf(checker)>=0){
+      hasWon = true;
     }
     if(diag1.indexOf(checker)>=0){
       hasWon = true;
@@ -130,7 +129,6 @@ public class Connect4{
     if(diag2.indexOf(checker)>=0){
       hasWon = true;
     }
-
     return hasWon;
   }
 
@@ -138,9 +136,9 @@ public class Connect4{
     String s = "";
     for(int i=0; i<6; i++){
       for(int j=0; j<7; j++){
-        s += board[i][j] + " ";
+        s += "|" + board[i][j];
       }
-      s+="\n";
+      s+="|\n";
     }
     return s;
   }
